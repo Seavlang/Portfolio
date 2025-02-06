c478f83b40843c17f8b0d064ef845c4d9fe49549
Seavlang <sievlangvey@gmail.com>
2025-02-06 13:38:22 +0700

    seavlang

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Hello this is my describtion.
+            Hello this is my seavlang describtion.
           </p>
           <div>
             <Link
